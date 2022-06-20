4d9ea3f613fd29f83084dd79c07f51f7b496f8b3
wandergithub <wandergonzalez0@hotmail.com>
2022-06-20 18:37:43 -0400

    Create Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
 import { getAvgStockPrices } from './redux/Market/Market';
 import Home from './components/Home';
+import Header from './components/Header';
 
 function App() {
   const dispatch = useDispatch();
@@ -11,10 +15,12 @@ function App() {
     dispatch(getAvgStockPrices(mostSearched));
   }, []);
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Details" element={<Home />} />
-    </Routes>
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <p>2022 Average Stock Prices</p>
+    </header>
+  );
+}
