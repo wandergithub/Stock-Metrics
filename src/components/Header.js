4c7951a4c99d936c6531f684a5fe4d90da1225e2
wandergithub <wandergonzalez0@hotmail.com>
2022-06-30 13:23:35 -0400

    update style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
+import SettingsIcon from '@mui/icons-material/Settings';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
-import backIcon from '../assets/ic_arrow_back.png';
-import settingsIcon from '../assets/settings.png';
 
 export default function Header() {
   return (
     <header>
       <Link to="/" data-testid="backBtn" className="header page-t">
-        <img className="back-i" src={backIcon} alt="Back" />
-        {window.location.pathname === '/' && <p>Stock Market</p>}
+        <ArrowBackIcon className="back-i" fontSize="large" />
+        {window.location.pathname === '/' && <Typography variant="h3" component="h1">Stock Market</Typography>}
         {window.location.pathname === '/details' && <p>Stock Market</p>}
-        <img className="settings-i" src={settingsIcon} alt="Setting" />
+        <SettingsIcon className="SettingsIcon" fontSize="large" />
       </Link>
     </header>
   );
