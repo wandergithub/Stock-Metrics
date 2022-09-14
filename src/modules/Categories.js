f1c0c5a131d6bcf0b22e0d218554664071c03d49
wandergithub <wandergonzalez0@hotmail.com>
2022-09-14 11:36:05 -0400

    Add new symbols

--- src/modules/Categories.js
+++ src/modules/Categories.js
@@ -1,6 +1,6 @@
 // Stocks
-const symbolList = ['AAPL', 'META', 'GOOG', 'MSFT', 'NVDA', 'WBA',
-  'CNVY', 'LEXX', 'REV', 'MREO', 'SOUN', 'SWVL',
+const symbolList = ['AAPL', 'META', 'GOOG', 'GOOGL', 'AMZN', 'UNH', 'JNJ', 'V', 'TSM', 'XOM', 'WMT', 'TSLA', 'MSFT', 'NVDA', 'WBA',
+  'CNVY', 'LEXX', 'REV', 'MREO', 'SOUN', 'SWVL', 'JPM', 'PG', 'CVX', 'MA', 'LLY', 'HD', 'BAC', 'KO', 'PFE',
   'ADXN', 'TBLT', 'ADN', 'GBNH', 'BON'];
 
 export default symbolList;
